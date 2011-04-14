45dba9d9ede475b439871a8c71d551f151e15bf3
Benjamin "balupton" Lupton <b@lupton.cc>
2011-04-15 02:26:31 +0800

    Hopefully fixed a bug when scrolling to body.

--- scripts/jquery.scrollto.js
+++ scripts/jquery.scrollto.js
@@ -8,7 +8,7 @@
  * jQuery Aliaser
  */
 (function($){
-	
+
 	/**
 	 * jQuery ScrollTo (balupton edition)
 	 * @version 1.0.1
@@ -30,20 +30,20 @@
 				callback: undefined,
 				durationMode: 'each'
 			},
-			
+
 			/**
 			 * Configure ScrollTo
 			 */
 			configure: function(options){
 				var ScrollTo = $.ScrollTo;
-				
+
 				// Apply Options to Config
 				$.extend(ScrollTo.config, options||{});
-				
+
 				// Chain
 				return this;
 			},
-		
+
 			/**
 			 * Perform the Scroll Animation for the Collections
 			 * We use $inline here, so we can determine the actual offset start for each overflow:scroll item
@@ -51,12 +51,12 @@
 			 */
 			scroll: function(collections, config){
 				var ScrollTo = $.ScrollTo;
-			
+
 				// Determine the Scroll
 	    		var	collection = collections.pop(),
 					$container = collection.$container,
 					$target = collection.$target;
-			
+
 				// Prepare the Inline Element of the Container
 				var $inline = $('<span/>').css({
 					'position': 'absolute',
@@ -64,20 +64,20 @@
 					'left': '0px'
 				});
 				var position = $container.css('position');
-			
+
 				// Insert the Inline Element of the Container
 				$container.css('position','relative');
 				$inline.appendTo($container);
-			
+
 				// Determine the Offsets
 				var	startOffset = $inline.offset().top,
 					targetOffset = $target.offset().top,
 					offsetDifference = targetOffset - startOffset;
-			
+
 				// Reset the Inline Element of the Container
 				$inline.remove();
 				$container.css('position',position);
-				
+
 				// Prepare the callback
 				var callback = function(event){
 					// Check
@@ -94,38 +94,38 @@
 					// Return true
 					return true;
 				};
-			
+
 				// Perform the Scroll
 				$container.animate({
 					'scrollTop': offsetDifference+'px'
 				}, config.duration, config.easing, callback);
-			
+
 				// Return true
 				return true;
 			},
-		
+
 			/**
 			 * ScrollTo the Element using the Options
 			 */
 			fn: function(options){
 				var ScrollTo = $.ScrollTo;
-				
+
 				// Prepare
 				var	$target = $(this);
 				if ( $target.length === 0 ) {
 					// Chain
 					return this;
 				}
-				
+
 				// Fetch
 				var	$container = $target.parent(),
 					collections = [];
-	
+
 				// Handle Options
 				config = $.extend({},ScrollTo.config,options);
 
 				// Cycle through the containers
-				while ( $container.length === 1 && !$container.is('body') ) {
+				while ( $container.length === 1 && !$container.is('body') && !($container.get(0) === document) ) {
 					// Check Container
 					var container = $container.get(0);
 					if ( $container.css('overflow-y') !== 'visible' && container.scrollHeight !== container.clientHeight ) {
@@ -140,47 +140,47 @@
 					// Update the Container
 					$container = $container.parent();
 				}
-			
+
 				// Add the final collection
 				collections.push({
 					'$container': $($.browser.msie ? 'html' : 'body'),
 					'$target': $target
 				});
-	
+
 				// Adjust the Config
 				if ( config.durationMode === 'all' ) {
 					config.duration /= collections.length;
 				}
-				
+
 				// Handle
 				ScrollTo.scroll(collections,config);
-			
+
 				// Chain
 				return this;
 			},
-		
+
 			/**
 			 * Construct
 			 */
 			construct: function(options){
 				var ScrollTo = $.ScrollTo;
-			
+
 				// Apply our jQuery Function
 				$.fn.ScrollTo = ScrollTo.fn;
-			
+
 				// Apply our Options to the Default Config
 				ScrollTo.config = $.extend(ScrollTo.config,options);
-			
+
 				// Chain
 				return this;
 			}
 		};
-	
+
 		// Construct It
 		$.ScrollTo.construct();
 	}
 	else {
 		window.console.warn("$.ScrollTo has already been defined...");
 	}
-	
+
 })(jQuery);
