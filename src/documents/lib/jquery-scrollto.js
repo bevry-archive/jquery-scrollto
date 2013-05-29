8aafe26219262907ee72b6e3a785fd7f791bb55d
Benjamin Lupton <b@lupton.cc>
2013-05-29 21:08:17 +0800

    Now uses the docpad umd plugin

--- src/documents/lib/jquery-scrollto.js
+++ src/documents/lib/jquery-scrollto.js
@@ -1,9 +1,8 @@
-/*global define:false require:false */
-(function (name, context, definition) {
-	if (typeof module != 'undefined' && module.exports) module.exports = definition();
-	else if (typeof define == 'function' && define.amd) define(definition);
-	else context[name] = definition();
-})('jquery-scrollto', this, function(){
+---
+umd: true
+---
+
+(function(){
 	// Prepare
 	var jQuery, $, ScrollTo;
 	jQuery = $ = window.jQuery || require('jquery');
@@ -258,4 +257,4 @@
 
 	// Export
 	return ScrollTo;
-});
+}).call(this);
