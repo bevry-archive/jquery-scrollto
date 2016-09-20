03faab701a7e8f36cfa01e68ab2d5a8754f6a1ab
Inumedia <inumedia@inumedia.net>
2016-09-20 19:56:51 -0400

    Switching out UMD tag for actual UMD code

--- src/documents/lib/jquery-scrollto.js
+++ src/documents/lib/jquery-scrollto.js
@@ -1,8 +1,10 @@
----
-umd: true
----
-
-(function(){
+/*global define:false require:false */
+(function (name, context, definition) {
+    if ( typeof module !== 'undefined' && module.exports ) module.exports = definition();
+    else if ( typeof define === 'function' && define.amd ) define(definition);
+    else if ( typeof provide === 'function' ) provide(name, definition());
+    else context[name] = definition();
+})('jquery-scrollto', this, function () {
 	// Prepare
 	var jQuery, $, ScrollTo;
 	jQuery = $ = window.jQuery || require('jquery');
@@ -257,4 +259,4 @@ umd: true
 
 	// Export
 	return ScrollTo;
-}).call(this);
+});
